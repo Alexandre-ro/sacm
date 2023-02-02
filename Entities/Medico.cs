ae912ed7229d8d5cbf40cab3ee252f33213ad735
Alexandre R Oliveira <alexandre.ri.oliveira@gmail.com>
2023-02-01 22:09:52 -0300

    Criado a entidade Medico.

---
+++ Entities/Medico.cs
@@ -0,0 +1,15 @@
+﻿namespace SACM.Entities
+{
+    public class Medico
+    {
+        private int Id { get; set; }
+        public int CodigoEspecialidade { get; set; }
+        public string Crm { get; set; }
+        public string Nome { get; set; }
+        public string InstituicaoFormatura { get; set; }
+        public int AnoFormatura { get; set; }
+        public int AnoNascimento   { get; set; }
+        public int MesNascimento { get; set; }
+        public int DiaNascimento { get; set; }
+    }
+}
